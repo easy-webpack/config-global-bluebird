2aa094b5ea46a9c0451a420b77623795f8daa7e6
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2016-07-28 19:19:08 +0200

    feat(index): expose Bluebird as global.Promise

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,23 @@
 import {WebpackConfig, get} from '@easy-webpack/core'
 import * as webpack from 'webpack'
 
-export = function bluebird() {
+export = function bluebird({expose = true}) {
   return function bluebird(this: WebpackConfig): WebpackConfig {
-    return {
+    const config = {
       plugins: [
         new webpack.ProvidePlugin({
           'Promise': 'bluebird',
         })
       ].concat(get(this, 'plugins', []))
+    } as WebpackConfig
+
+    if (expose) {
+      config.module = {
+        loaders: get(this, 'module.loaders', []).concat([
+          { test: require.resolve('bluebird'), loader: 'expose?Promise' }
+        ])
+      }
     }
+    return config
   }
 }
