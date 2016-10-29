51d46e6b3936c12fb637a13bb8a93b2a266204a5
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2016-10-29 03:38:13 +0200

    fix(index): use the type with metadata

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import {WebpackConfig, get} from '@easy-webpack/core'
+import {WebpackConfigWithMetadata, get} from '@easy-webpack/core'
 import * as webpack from 'webpack'
 
 export = function bluebird({expose = true} = {}) {
-  return function bluebird(this: WebpackConfig): WebpackConfig {
+  return function bluebird(this: WebpackConfigWithMetadata): WebpackConfigWithMetadata {
     const config = {
       plugins: [
         new webpack.ProvidePlugin({
           'Promise': 'bluebird',
         })
       ].concat(get(this, 'plugins', []))
-    } as WebpackConfig
+    } as WebpackConfigWithMetadata
 
     if (expose) {
       config.module = {
